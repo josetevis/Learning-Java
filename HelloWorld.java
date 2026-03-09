d2f91ecb1a9bb4e7280ea4e824c4e53ed0daa3f2
josetevis <josetevis34@gmail.com>
2026-03-09 17:34:27 -0300

    Fix: raparigas

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("1 Rapariga é bom, 3 raparigas é bom demais!");
     }
 }
